84e5ce2a6b8199e83d11cb7d70f6f3116068d357
DCisero <deshaylacisero@gmail.com>
2019-02-11 20:08:45 -0500

    code refactor and clean up

--- src/main/java/BabySitter.java
+++ src/main/java/BabySitter.java
@@ -3,20 +3,20 @@ public class BabySitter {
 
     private final int FAMILY_A_PAY_BEFORE11 = 15;
     private final int FAMILY_A_PAY_AFTER11 = 20;
-    private int totalHoursWorkedUntil11 = 6;
-    private int totalHoursWorkedAfter11 = 5;
+    private int[] hoursA = {5,6,7,8,9,10};
+    private int[] hoursA2 = {11,12,1,2,3,4};
 
     private final int FAMILY_B_PAY_BEFORE10 = 12;
     private final int FAMILY_B_PAY_AFTER10_UNTIL12 = 8;
     private final int FAMILY_B_PAY_AFTER12 = 16;
-    private int totalHoursWorkedUntil10 = 5;
-    private int totalHoursWorked10Until12 = 2;
-    private int totalHoursWorkedAfter12 = 4;
+    private int[] hoursB = {5,6,7,8,9};
+    private int[] hoursB2 = {10,11};
+    private int[] hoursB3 = {12,1,2,3};
 
     private final int FAMILY_C_PAY_BEFORE9 = 21;
     private final int FAMILY_C_PAY_AFTER9 = 15;
-    private int totalHoursWorkedUntil9 = 4;
-    private int totalHoursWorkedAfter9 = 7;
+    private int[] hoursC = {5,6,7,8};
+    private int[] hoursC2 = {9,10,11,12,1,2,3};
 
     public int shiftPayment;
     public int shiftPayment2;
@@ -25,7 +25,6 @@ public class BabySitter {
     public int totalPayA;
     public int totalPayB;
     public int totalPayC;
-
 
     public boolean shiftTime(double startTime, double endTime) {
         if (startTime == 5.00 && endTime == 4.00) {
@@ -44,57 +43,65 @@ public class BabySitter {
         return false;
     }
 
-    public int familyAPayBefore11(int shiftHours) {
-        if (shiftHours <= totalHoursWorkedUntil11) {
-            shiftPayment = FAMILY_A_PAY_BEFORE11 * shiftHours;
+    public int familyAPayBefore11(int[] shiftHours) {
+        for (int i = 0; i <= shiftHours.length; i++) {
+            if (shiftHours.equals(i) == hoursA.equals(i)) {
+                shiftPayment = FAMILY_A_PAY_BEFORE11 * shiftHours.length;
+            }
         }
         return shiftPayment;
     }
 
-    public int familyAPayAfter11(int shiftHours) {
-        if (shiftHours <= totalHoursWorkedAfter11){
-            shiftPayment2 = FAMILY_A_PAY_AFTER11 * shiftHours;
+    public int familyAPayAfter11(int[] shiftHours) {
+        for (int i = 0; i <= shiftHours.length; i++) {
+            if (shiftHours.equals(i) == hoursA2.equals(i)) {
+                shiftPayment2 = FAMILY_A_PAY_AFTER11 * shiftHours.length;
+            }
         }
         return totalPayA = shiftPayment2 + shiftPayment;
     }
 
-    public int familyBPayUntil10(int shiftHours) {
-        if (shiftHours <= totalHoursWorkedUntil10){
-            shiftPayment = FAMILY_B_PAY_BEFORE10 * shiftHours;
-        }
-
+    public int familyBPayUntil10(int[] shiftHours) {
+        for (int i = 0; i <= shiftHours.length; i++)
+            if (shiftHours.equals(i) == hoursB.equals(i)){
+                shiftPayment = FAMILY_B_PAY_BEFORE10 * shiftHours.length;
+            }
         return shiftPayment;
     }
 
-    public int familyBPayUntil12(int shiftHours) {
-        if (shiftHours <= totalHoursWorked10Until12){
-            shiftPayment2 = FAMILY_B_PAY_AFTER10_UNTIL12 * shiftHours;
-        }
+    public int familyBPayUntil12(int[] shiftHours) {
+        for (int i = 0; i <= shiftHours.length; i++)
+            if (shiftHours.equals(i) == hoursB2.equals(i)){
+                shiftPayment2 = FAMILY_B_PAY_AFTER10_UNTIL12 * shiftHours.length;
+            }
         return shiftPayment2;
     }
 
-    public int familyBPayUntilEnd(int shiftHours) {
-        if (shiftHours <= totalHoursWorkedAfter12){
-            shiftPayment3 = FAMILY_B_PAY_AFTER12 * shiftHours;
-        }
+    public int familyBPayUntilEnd(int[] shiftHours) {
+        for (int i = 0; i <= shiftHours.length; i++)
+            if (shiftHours.equals(i) == hoursB3.equals(i)){
+                shiftPayment3 = FAMILY_B_PAY_AFTER12 * shiftHours.length;
+            }
         return totalPayB = shiftPayment3 + shiftPayment2 + shiftPayment;
     }
 
-    public int familyCPayUntil9(int shiftHours) {
-        if (shiftHours <= totalHoursWorkedUntil9){
-            shiftPayment = FAMILY_C_PAY_BEFORE9 * shiftHours;
-        }
+    public int familyCPayUntil9(int[] shiftHours) {
+        for (int i = 0; i <= shiftHours.length; i++)
+            if (shiftHours.equals(i) == hoursC.equals(i)){
+                shiftPayment = FAMILY_C_PAY_BEFORE9 * shiftHours.length;
+            }
         return shiftPayment;
     }
 
-    public int familyCPayAfter9(int shiftHours) {
-        if (shiftHours <= totalHoursWorkedAfter9){
-            shiftPayment2 = FAMILY_C_PAY_AFTER9 * shiftHours;
-        }
+    public int familyCPayAfter9(int[] shiftHours) {
+        for (int i = 0; i <= shiftHours.length; i++)
+            if (shiftHours.equals(i) == hoursC2.equals(i)){
+                shiftPayment2 = FAMILY_C_PAY_AFTER9 * shiftHours.length;
+            }
         return totalPayC = shiftPayment2 + shiftPayment;
     }
 
-    }
+}
 
 
 
--- src/test/java/BabySitterTest.java
+++ src/test/java/BabySitterTest.java
@@ -12,15 +12,15 @@ import static org.junit.Assert.*;
             sitter = new BabySitter();
         }
 
-        //start time 5 and end time 4
         @Test
-        public void StartTimeIsNoEarlierThan5Pm_EndTimeIsNoLaterThan4Am() {
+        public void StartTimeIsNoEarlierThan5Pm_EndTimeIsNoLaterThan4Am(){
 
-            assertTrue(sitter.shiftTime(5.00, 4.00));
-            assertFalse(sitter.shiftTime(3.00, 8.30));
-            assertFalse(sitter.shiftTime(1.00, 10.30));
-            assertFalse(sitter.shiftTime(4.00, 6.30));
+            assertTrue(sitter.shiftTime(5.00,4.00));
+            assertFalse(sitter.shiftTime(3.00,8.30));
+            assertFalse(sitter.shiftTime(1.00,10.30));
+            assertFalse(sitter.shiftTime(4.00,6.30));
         }
+
 
         @Test
         public void ValidateOneFamilyPerNight(){
@@ -32,7 +32,6 @@ import static org.junit.Assert.*;
             assertTrue(familyPerNight);
             assertFalse(sitter.contains(2));
             assertFalse(sitter.contains(3));
-
         }
 
         //Family A pays $15 per hour before 11pm
@@ -42,12 +41,11 @@ import static org.junit.Assert.*;
             sitter.shiftTime(5,11);
             sitter.shiftTime(6,10);
 
-            int shiftHours = 6;
-            int shiftHours2 = 4;
+            int[] shiftHours = {5,6,7,8,9,10};
+            int[] shiftHours2 = {6,7,8,9,10};
 
             assertEquals(90, sitter.familyAPayBefore11(shiftHours));
-            assertEquals(60, sitter.familyAPayBefore11(shiftHours2));
-
+            assertEquals(75, sitter.familyAPayBefore11(shiftHours2));
         }
 
         //Family A pays $20 per hour after 11pm
@@ -57,12 +55,12 @@ import static org.junit.Assert.*;
             sitter.shiftTime(11,2);
             sitter.shiftTime(12,4);
 
-            int shiftHours = 3;
-            int shiftHours2 = 4;
+            int[] shiftHours = {11,12,1,2};
+            int[] shiftHours2 = {12,1,2,3,4};
 
-            assertEquals(60, sitter.familyAPayAfter11(shiftHours));
-            assertEquals(80, sitter.familyAPayAfter11(shiftHours2));
 
+            assertEquals(80, sitter.familyAPayAfter11(shiftHours));
+            assertEquals(100, sitter.familyAPayAfter11(shiftHours2));
         }
 
         //Family B pays $12 per hour before 10pm
@@ -72,24 +70,22 @@ import static org.junit.Assert.*;
             sitter.shiftTime(5,10);
             sitter.shiftTime(7,10);
 
-            int shiftHours = 5;
-            int shiftHours2 = 3;
+            int[] shiftHours = {5,6,7,8,9};
+            int[] shiftHours2 = {7,8,9};
 
             assertEquals(60, sitter.familyBPayUntil10(shiftHours));
             assertEquals(36, sitter.familyBPayUntil10(shiftHours2));
-
         }
 
-        //Family B pays $8 per hour between 10pm and 12am
+        //Family B pays $8 per hour between 10pm-12am
         @Test
         public void FamilyBPay10Until12(){
 
             sitter.shiftTime(10,12);
 
-            int shiftHours = 2;
+            int[] shiftHours = {10,11};
 
             assertEquals(16, sitter.familyBPayUntil12(shiftHours));
-
         }
 
         //Family B pays $16 per hour after 12am
@@ -99,12 +95,11 @@ import static org.junit.Assert.*;
             sitter.shiftTime(12,4);
             sitter.shiftTime(12,2);
 
-            int shiftHours = 4;
-            int shiftHours2 = 2;
+            int[] shiftHours = {12,1,2,3};
+            int[] shiftHours2 = {12,2};
 
             assertEquals(64, sitter.familyBPayUntilEnd(shiftHours));
             assertEquals(32, sitter.familyBPayUntilEnd(shiftHours2));
-
         }
 
         //Family C pays $21 per hour before 9pm
@@ -114,12 +109,11 @@ import static org.junit.Assert.*;
             sitter.shiftTime(5,9);
             sitter.shiftTime(7,9);
 
-            int shiftHours = 4;
-            int shiftHours2 = 2;
+            int[] shiftHours = {5,6,7,8};
+            int[] shiftHours2 = {7,8};
 
             assertEquals(84, sitter.familyCPayUntil9(shiftHours));
             assertEquals(42, sitter.familyCPayUntil9(shiftHours2));
-
         }
 
         //Family C pays $15 per hour after 9pm
@@ -128,14 +122,16 @@ import static org.junit.Assert.*;
 
             sitter.shiftTime(9,4);
             sitter.shiftTime(9,1);
+            sitter.shiftTime(9,12);
 
-            int shiftHours = 7;
-            int shiftHours2 = 4;
+            int[] shiftHours = {9,10,11,12,1,2,3};
+            int[] shiftHours2 = {9,10,11,12};
+            int[] shiftHours3 = {9,10,11};
 
             assertEquals(105, sitter.familyCPayAfter9(shiftHours));
             assertEquals(60, sitter.familyCPayAfter9(shiftHours2));
+            assertEquals(45, sitter.familyCPayAfter9(shiftHours3));
         }
-
 
     }
 
